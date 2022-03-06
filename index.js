f5429eecf344f14c5e3c0e1928bf661d5997a3b2
Joshua Liu <supajoshjunk@gmail.com>
2022-03-06 13:29:00 +0000

    high score system

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const app = express();
 const server = app.listen(process.env.PORT || 3000, () => console.log('listening on port ' + (process.env.PORT || 3000).toString()));
 console.log('starting up')
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.use(express.static(path.join(__dirname, '/site')));
 app.use((req, res, next) => {
-	if (req.baseUrl === '/views') {
-		next();
-		return;
-	}
 	const data = parseInt(fs.readFileSync(path.join(__dirname, '/views.txt')));
 	fs.writeFileSync(path.join(__dirname, '/views.txt'), (data + 1).toString());
 	next()
@@ -18,6 +17,29 @@ app.use((req, res, next) => {
 
 app.get('/views', (req, res) => {
 	res.send('<h1>' + fs.readFileSync(path.join(__dirname, '/views.txt')) + '</h1>');
+});
+
+app.get('/records/:game', (req, res) => {
+	const { game } = req.params;
+	let currentWr = require('./records.json');
+	if (currentWr[game] !== undefined) {
+		res.send(JSON.stringify(currentWr[game]));
+	} else {
+		res.send('false');
+	}
+});
+
+app.post('/records/:game/:type/:record', (req, res) => {
+	const { game, type, record } = req.params;
+	let currentWr = require('./records.json');
+	if (typeof currentWr[game] !== 'undefined' && typeof currentWr[game][type] !== 'undefined' && typeof record !== 'undefined' && currentWr[game][type] < parseInt(record)) {
+		currentWr[game][type] = parseInt(record);
+		fs.writeFileSync('./records.json', JSON.stringify(currentWr, null, 2));
+		console.log('record')
+		res.send('true');
+	} else {
+		res.send('false');
+	}
 });
 
 console.log('done')
